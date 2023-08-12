bdb8ad831e39f8a31cc2d8ae5e9a0f2dabe6c2f7
pro100bat9 <doroshenko.simon@mail.ru>
2023-08-12 21:57:06 +0300

    "refactor: delete useless code"

--- migrations/scripts/01-08-2023-CRS-1_create_tg_chat_table.sql
+++ migrations/scripts/01-08-2023-CRS-1_create_tg_chat_table.sql
@@ -1,9 +1,5 @@
 --liquibase formatted sql
 
--- --changeset pro100bat9:create_chat_sequence
--- CREATE Sequence chat_id_sequence;
-
-
 --changeset pro100bat9:create_chat_table
 CREATE TABLE IF NOT EXISTS tg_chat
 (
